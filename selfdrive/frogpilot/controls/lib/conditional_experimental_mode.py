mike86437/openpilot

Controls - Conditional Experimental Mode - Navigation Based
Switch to 'Experimental Mode' based on navigation data. (i.e. Intersections, stop signs, upcoming turns, etc.).

Co-Authored-By: mike8643 <98910897+mike8643@users.noreply.github.com>

selfdrive/frogpilot/controls/lib/conditional_experimental_mode.py
@@ -25,6 +25,11 @@ class ConditionalExperimentalMode:
       self.status_value = 7 if lead_status else 8
       return True
 
+    approaching_maneuver = modelData.navEnabled and (frogpilotNavigation.approachingIntersection or frogpilotNavigation.approachingTurn)
+    if frogpilot_toggles.conditional_navigation and approaching_maneuver and (frogpilot_toggles.conditional_navigation_lead or not lead_status):
+      self.status_value = 10 if frogpilotNavigation.approachingIntersection else 11
+      return True
+
     if frogpilot_toggles.conditional_curves and self.curve_detected:
       self.status_value = 12
       return True

mikehundley/fp

selfdrive/frogpilot/controls/lib/conditional_experimental_mode.py
@@ -29,6 +29,11 @@ class ConditionalExperimentalMode:
       self.status_value = 7 if tracking_lead else 8
       return True
 
+    approaching_maneuver = modelData.navEnabled and (frogpilotNavigation.approachingIntersection or frogpilotNavigation.approachingTurn)
+    if frogpilot_toggles.conditional_navigation and approaching_maneuver and (frogpilot_toggles.conditional_navigation_lead or not tracking_lead):
+      self.status_value = 10 if frogpilotNavigation.approachingIntersection else 11
+      return True
+
     if frogpilot_toggles.conditional_curves and self.curve_detected and (frogpilot_toggles.conditional_curves_lead or not tracking_lead):
       self.status_value = 12
       return True